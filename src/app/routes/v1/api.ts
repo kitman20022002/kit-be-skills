8041ffcae0233174a21c03dcb37fb58afb01c7be
kitman <kitman200220022002@gmail.com>
2025-04-25 00:21:50 +1000

    fix package.json

--- src/app/routes/v1/api.ts
+++ src/app/routes/v1/api.ts
@@ -9,32 +9,32 @@ import * as blogController from '../../controllers/v1/blogController';
 
 const router = express.Router();
 
-function asyncMiddleware(fn: any) {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((e) => {
-      console.error(e.message);
-      logger.error(e.message);
-      next();
-    });
-  };
-}
+// function asyncMiddleware(fn: any) {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     Promise.resolve(fn(req, res, next)).catch((e) => {
+//       console.error(e.message);
+//       logger.error(e.message);
+//       next();
+//     });
+//   };
+// }
 
-router.use((req: Request, res: Response, next: NextFunction) => {
-  try {
-    for (const layer of router.stack) {
-      if (layer.route) {
-        for (const method in layer.route.methods) {
-          if (layer.route.methods[method]) {
-            layer.route.methods[method] = asyncMiddleware(layer.route.methods[method]);
-          }
-        }
-      }
-    }
-    next();
-  } catch (e) {
-    console.error('fxx i');
-  }
-});
+// router.use((req: Request, res: Response, next: NextFunction) => {
+//   try {
+//     for (const layer of router.stack) {
+//       if (layer.route) {
+//         for (const method in layer.route.methods) {
+//           if (layer.route.methods[method]) {
+//             layer.route.methods[method] = asyncMiddleware(layer.route.methods[method]);
+//           }
+//         }
+//       }
+//     }
+//     next();
+//   } catch (e) {
+//     console.error('fxx i');
+//   }
+// });
 
 router.get('/up', (req: Request, res: Response) => {
   res.sendStatus(200);
