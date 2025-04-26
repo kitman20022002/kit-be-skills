01eabbadd26a06835abc7a0c2d7644ac0b293e4a
kitman <kitman200220022002@gmail.com>
2025-04-26 17:50:29 +1000

    add pacakage

--- index.ts
+++ index.ts
@@ -1,7 +1,4 @@
 export {};
-const mongoose = require('mongoose');
-const mongoosePlugin = require('./src/loaders/mongoose-plugin/renameId');
-mongoose.plugin(mongoosePlugin.plugin);
 
 const loader = require('./src/loaders');
 loader.init();
